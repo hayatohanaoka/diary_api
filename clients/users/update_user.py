676783cbbe0173ed98791f2e46c2ec864f03e9e7
hayatohanaoka <h.hanaoka@mediwel.net>
2024-08-26 17:17:36 +0900

    Add client python files for test users CRUD

--- clients/users/update_user.py
+++ clients/users/update_user.py
@@ -1,16 +1,15 @@
 import requests
 
-from token_login_user import get_login_token
+from diary_api.clients.token_login_user import get_login_token
 
-login_token_json = get_login_token('aav@bbb.ccc', '123456789')
-token = login_token_json['token']
+token = get_login_token('aa@bbb.ccc', '123456789')
 
 res = requests.patch(
-    'http://127.0.0.1:8000/api/v1/users/1/',
+    'http://127.0.0.1:8000/api/v1/users/2/',
     data={
         'username': 'test_user',
-        'nickname': 'test_nickname',
-        'email': 'a@bbb.ccc',
+        'nickname': 'test_nickname_',
+        'email': 'aa@bbb.ccc',
     },
     headers={
         'Authorization': f'Token {token}'
